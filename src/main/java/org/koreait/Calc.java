dbc8aa763ee5e4ba66ab1781529d6b4cda18e962
hrjo0120 <hrjo0120@gmail.com>
2024-06-25 11:36:26 +0900

    10 + 5 * 2

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,29 +1,40 @@
 package org.koreait;
 
+import java.util.Arrays;
+
 public class Calc {
     public static int run(String exp) {
 
         boolean needPlus = exp.contains("+");
         boolean needMulti = exp.contains("*");
 
-        if(needPlus) {
+        boolean needToCompound = needPlus && needMulti;   //섞여있어?!
+
+        if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            return Integer.parseInt(bits[0]) + run(bits[1]);    //내가 나를  실행. 재귀함수 기초
+
+        }
+
+        if (needPlus) {
             exp = exp.replaceAll("- ", "+ -");// -로 들어온 경우 + -로 치환
 
             String[] bits = exp.split(" \\+ "); // + 로 자름
 
             int sum = 0;
 
-            for(int i = 0; i< bits.length; i++) {
+            for (int i = 0; i < bits.length; i++) {
                 sum += Integer.parseInt(bits[i]);
             }
 
             return sum;
-        } else if(needMulti) {
+        } else if (needMulti) {
             String[] bits = exp.split(" \\* ");
 
             int sum = 1;
 
-            for(int i = 0; i< bits.length; i++) {
+            for (int i = 0; i < bits.length; i++) {
                 sum *= Integer.parseInt(bits[i]);
             }
 
