0f266f46b3b85fb352f10879dfd66f083eb69063
hrjo0120 <hrjo0120@gmail.com>
2024-06-25 10:24:03 +0900

    10 * 10 * 10

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -3,18 +3,34 @@ package org.koreait;
 public class Calc {
     public static int run(String exp) {
 
-        exp = exp.replaceAll("- ", "+ -");// -로 들어온 경우 + -로 치환
+        boolean needPlus = exp.contains("+");
+        boolean needMulti = exp.contains("*");
 
-        String[] bits = exp.split(" \\+ ");
+        if(needPlus) {
+            exp = exp.replaceAll("- ", "+ -");// -로 들어온 경우 + -로 치환
 
-        int sum = 0;
+            String[] bits = exp.split(" \\+ "); // + 로 자름
 
-        for(int i = 0; i< bits.length; i++) {
-            sum += Integer.parseInt(bits[i]);
+            int sum = 0;
+
+            for(int i = 0; i< bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if(needMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for(int i = 0; i< bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
         }
-        return sum;
 
-//        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
 
     }
 }
