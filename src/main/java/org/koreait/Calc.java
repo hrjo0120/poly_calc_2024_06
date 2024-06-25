8ad34cdc9e6f34bb6fb357c08cbeadada7cc3949
hrjo0120 <hrjo0120@gmail.com>
2024-06-25 13:56:47 +0900

    20 + 10 + 5 * 2, 10 * 20 + 10 + 5 * 2

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,20 +1,29 @@
 package org.koreait;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Calc {
     public static int run(String exp) {
+        // 단일 항이 들어오면 바로 리턴
+        if(!exp.contains(" ")){ //" " 공백이 있는지 없는지 체크
+            return Integer.parseInt(exp);
+        }
 
         boolean needPlus = exp.contains("+");
         boolean needMulti = exp.contains("*");
 
         boolean needToCompound = needPlus && needMulti;   //섞여있어?!
 
-        if (needToCompound) {
-            String[] bits = exp.split(" \\+ ");
+        if (needToCompound) {   //True일때 실행.
+            String[] bits = exp.split(" \\+ "); //{"20", "10", "5 * 2"}
 
-            return Integer.parseInt(bits[0]) + run(bits[1]);    //내가 나를  실행. 재귀함수 기초
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)        //map: 1대1 대응 하는것. 정수화 하는 것. , run을 실행
+                    .mapToObj(e -> e + "")      //객체가 아닌데 객체로 바꿔야될 때
+                    .collect(Collectors.joining(" + "));    // " + "로 엮는다.
 
+            return run(newExp);         //재귀함수, 처음으로 돌아가서 값을 처리함.
         }
 
         if (needPlus) {
